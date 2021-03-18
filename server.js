8a356eac37cac3c1fc2c4d1fb4d00b60f78bfda1
Dhanish Kumar <dhanish@mobiux.in>
2021-03-18 18:43:39 +0530

    server.js currently takes care of serving posts and authenticating the provided token with the request

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+const PORT = process.env.POSTS_SERVER_PORT || 5000;
+
+const posts = [{
+  username: 'Dhanish',
+  title: 'Post 1'
+}, {
+  username: 'Kumar',
+  title: 'Post 2'
+}];
+
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const  token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(PORT);
